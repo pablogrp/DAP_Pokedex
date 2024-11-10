def7582beb1456a70b7bec9c8dd9ae274fd6823e
Daniel del Rosario <danidelrosario7@gmail.com>
2024-11-10 01:11:40 +0000

    Ninth Generation Factory and products created

---
+++ src/NinthGenBerry.java
@@ -0,0 +1,22 @@
+public class NinthGenBerry implements Berry {
+
+    @Override
+    public String getName() {
+        return "";
+    }
+
+    @Override
+    public String getSprite() {
+        return "";
+    }
+
+    @Override
+    public String getEffect() {
+        return "";
+    }
+
+    @Override
+    public int getGrowthTime() {
+        return 0;
+    }
+}

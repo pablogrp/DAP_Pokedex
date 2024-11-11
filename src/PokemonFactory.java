bae27ee66c00a7ecfff69001aea2ae08da6f3620
PedroHdez18 <alu0101423944@ull.edu.es>
2024-11-11 19:56:18 +0000

    createBerry(..)

--- src/PokemonFactory.java
+++ src/PokemonFactory.java
@@ -5,4 +5,5 @@ public interface PokemonFactory {
 
     Pokemon createPokemon(String name, String sprite, int[] stats, String[] types, String mechanic);
     Pokeball createPokeball(String name, String sprite, String effect, int price);
+    Berry createBerry(String name, String sprite, String effect, int growthTime);
 }

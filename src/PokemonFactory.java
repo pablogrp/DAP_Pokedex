3a7d92cd83a7ca8575bd02998dfd41eb045973d8
PedroHdez18 <alu0101423944@ull.edu.es>
2024-11-13 18:58:25 +0000

    Deleting 1 attribute

--- src/PokemonFactory.java
+++ src/PokemonFactory.java
@@ -1,9 +1,5 @@
 public interface PokemonFactory {
-//    Pokemon[] createPokemons();
-//    Pokeball[] createPokeballs();
-//    Berry[] createBerrys();
-
-    Pokemon createPokemon(String name, String sprite, int[] stats, String[] types, String mechanic);
+    Pokemon createPokemon(String name, String sprite, int[] stats, String[] types);
     Pokeball createPokeball(String name, String sprite, String effect, int price);
     Berry createBerry(String name, String sprite, String effect, int growthTime);
 }
